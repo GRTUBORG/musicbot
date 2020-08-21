b44c6e69fbf8271df63c82098567613e13075cc2
GRTUBORG <68663095+GRTUBORG@users.noreply.github.com>
2020-08-22 02:53:04 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -159,8 +159,8 @@ async def play(ctx, url):  #КОМАНДА ПРОИГРЫВАНИЯ ЗВУКОВ
             name = file
             os.rename(file, 'song.mp3') 
     voice.play(discord.FFmpegPCMAudio('song.mp3'), after = lambda e: print(f'[logs] {name}, музыка закончила своё проигрывание'))
-    nname = name.rsplit("-", 2)
-    embed = discord.Embed(description = f'Сейчас играет: {nname[0]}', color = 0x4ace40)
+    
+    embed = discord.Embed(description = f'Сейчас играет: {name[0]}', color = 0x4ace40)
     embed.set_footer(text = "supports by quantprod")
     await ctx.send(embed = embed)
     
