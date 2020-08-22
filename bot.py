c58089a4447936f49128eaeb59d6d5b6de96c2cd
GRTUBORG <68663095+GRTUBORG@users.noreply.github.com>
2020-08-22 21:31:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -210,38 +210,6 @@ async def pause(ctx):
         embed.set_footer(text = "supports by quantprod")
         await ctx.send(embed = embed)    
         
-
-        
-#ПОГОДА
-
-@Bot.command()
-async def weather(ctx, *, city: str):
-    url = 'http://api.openweathermap.org/data/2.5/weather'
-    api_owm = '610db95b52ef9995408f455131444af3'
-    city_name = city
-    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
-    response = requests.get(complete_url)
-    x = response.json()
-    channel = ctx.message.channel
-    if x["cod"] != "404":
-        async with channel.typing():
-            y = x["main"]
-            current_temperature = y["temp"]
-            current_temperature_celsiuis = str(round(current_temperature - 273.15))
-            current_pressure = y["pressure"]
-            current_humidity = y["humidity"]
-            z = x["weather"]
-            weather_description = z[0]["description"]
-            embed = discord.Embed(title=f"Weather in {city_name}", color=ctx.guild.me.top_role.color, timestamp=ctx.message.created_at)
-            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
-            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
-            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
-            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
-            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
-            embed.set_footer(text=f"Requested by {ctx.author.name}")  
-            await channel.send(embed=embed)
-    else:
-        await channel.send("City not found.")
             
                  
 #ПОМОЩЬ USER
