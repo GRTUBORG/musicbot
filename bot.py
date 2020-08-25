082e729bb15a35dbb6bab179016de25c5c6b5d1f
GRTUBORG <68663095+GRTUBORG@users.noreply.github.com>
2020-08-25 23:30:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -441,11 +441,12 @@ async def math(ctx,  a:  int,  b:  int):
 #ИНФОРМАЦИЯ О ЮЗЕРЕ
 
 @Bot.command(aliases = ['i', 'information', 'INFO', 'INFORMATION'])
-async def info(ctx, member: discord.Member):  
+async def info(ctx, member: discord.Member): 
+    roles = [role.mention for role in roles[1:]]
     embed = discord.Embed(title = "Info", color = 0x428325)
     embed.add_field(name = "Когда присоединился: ", value = member.joined_at)
     embed.add_field(name = "Имя юзера: ", value = member.display_name)
-    embed.add_field(name = "Роли имеет: ", value = ' '.join([role.mention for role in roles[1:]]))
+    embed.add_field(name = "Роли имеет: ", value = ' '.join(roles), inline = True)
     embed.set_thumbnail(url = member.avatar_url)
     embed.set_footer(text = "supports by quantprod")
     await ctx.send(embed = embed)
